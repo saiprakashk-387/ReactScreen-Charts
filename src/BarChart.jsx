0a186a02150eaed04a3c60ec36fe313f0d81ced5
saiprakashk-387 <prakash387@yahoo.com>
2022-02-19 11:18:37 +0530

    React Charts

--- src/BarChart.jsx
+++ src/BarChart.jsx
@@ -16,41 +16,7 @@ useEffect(() => {
     })
   }
   const data = firstValue;
-  // const data =  [
-  //   {
-  //     type: 'jan',
-  //     sales: 38,
-  //   },
-  //   {
-  //     type: 'feb',
-  //     sales: 52,
-  //   },
-  //   {
-  //     type: 'march',
-  //     sales: 61,
-  //   },
-  //   {
-  //     type: 'april',
-  //     sales: 145,
-  //   },
-  //   {
-  //     type: 'may',
-  //     sales: 48,
-  //   },
-  //   {
-  //     type: 'june',
-  //     sales: 38,
-  //   },
-  //   {
-  //     type: 'july',
-  //     sales: 38,
-  //   },
-  //   {
-  //     type: 'august',
-  //     sales: 38,
-  //   },
-  // ];
-  const config = {
+   const config = {
     data,
     xField: 'sales',
     yField: 'type',
--- src/PieChart.jsx
+++ src/PieChart.jsx
@@ -17,32 +17,6 @@ const PieChart = () => {
     })
   }
   const data = first;
-  // const data =  [
-  //   {
-  //     type: 'Airtel',
-  //     value: 27,
-  //   },
-  //   {
-  //     type: 'Jio',
-  //     value: 25,
-  //   },
-  //   {
-  //     type: 'Bsnl',
-  //     value: 18,
-  //   },
-  //   {
-  //     type: 'Vi',
-  //     value: 15,
-  //   },
-  //   {
-  //     type: 'Network',
-  //     value: 10,
-  //   },
-  //   {
-  //     type: 'Net',
-  //     value: 5,
-  //   },
-  // ];
   const config = {
     appendPadding: 10,
     data,
